159293b761e7331bf07a58bfae6d20b8010e2168
DmitryPok <d.poklonov@mail.ru>
2017-10-14 13:11:23 +0300

    1.5. Checkstyle [#4123]
    1.5. Checkstyle [#4123]

--- chapter_001/src/test/java/ru/ru.job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/ru.job4j/CalculateTest.java
@@ -1,8 +1,6 @@
 package ru.job4j;
 
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -20,8 +18,8 @@ public class CalculateTest {
     @Test
     public void whenTakeNameThenTreeEchoPlusName() {
 		String input = "Petr Arsentev";
-		String expect = "Echo, echo, echo : Petr Arsentev"; 
-		Calculate calc = new Calculate();
+		String expect = "Echo, echo, echo : Petr Arsentev";
+	    Calculate calc = new Calculate();
 		String result = calc.echo(input);
 		assertThat(result, is(expect));
 	}
