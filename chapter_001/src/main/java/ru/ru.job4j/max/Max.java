0fe6f55d5ea218163421cd56132b442f3218a441
DmitryPok <d.poklonov@mail.ru>
2017-10-20 13:54:10 +0300

    3.1. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/ru.job4j/max/Max.java
@@ -0,0 +1,22 @@
+package ru.job4j.max;
+
+/**
+ * Calculate.
+ *
+ * @author Dmitry Poklonov (d.poklonov@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Max {
+    /**
+     * try to equal two int.
+     * @param first first
+     * @param second first
+     * @return return max value
+     */
+    public int max(int first, int second) {
+        return (first > second) ?  first : second;
+    }
+
+        }
