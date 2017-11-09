8b08f1975e5b1b5c9f1dc498b3683acf890998e8
DmitryPok <d.poklonov@mail.ru>
2017-11-09 19:47:09 +0300

    Проверка, что одно слово находится в другом слове [#228][#30277]

--- chapter_001/src/main/java/ru/ru.job4j/array/CheckWords.java
+++ chapter_001/src/main/java/ru/ru.job4j/array/CheckWords.java
@@ -1,6 +1,5 @@
 package ru.ru.job4j.array;
 
-import java.util.Arrays;
 /**
  * array.
  *
@@ -12,44 +11,22 @@ public class  CheckWords {
     /**
      * Array duplicate.
      *
-     * @param strin strin.
-     * @return return array value.
-     */
-
-    public char[] createArr(String strin) {
-        char[] array = {};
-        array = strin.toCharArray();
-        return array;
-    }
-
-    /**
-     * Array duplicate.
-     *
-     * @param arrSmall array.
-     * @param arrBig   array.
+     * @param smallWord array.
+     * @param bigWord   array.
      * @return return boolean value.
      */
-    public boolean innerCheck(char[] arrSmall, char[] arrBig) {
-        boolean[] verd = new boolean[arrSmall.length];
-        boolean verdict = true;
-        for (int ind = 0; ind < arrSmall.length; ind++) {
-            for (int indx = 0; indx < arrBig.length; indx++) {
-                if (arrSmall[ind] == arrBig[indx]) {
-                    verd[ind] = true;
-                    break;
-                } else {
-                    verd[ind] = false;
+    public boolean innerCheck(char[] smallWord, char[] bigWord) {
+        int wordCheker = 0;
+        for (int ind = 0; ind < smallWord.length; ind++) {
+            for (int indx = 0; indx < bigWord.length; indx++) {
+                if (smallWord[ind] == bigWord[indx]) {
+                    wordCheker++;
+                    if (wordCheker == smallWord.length) {
+                        return true;
+                    }
                 }
             }
         }
-        for (int ind = 0; ind < verd.length; ind++) {
-            if (verd[ind]) {
-                verdict = verd[ind];
-                break;
-            } else {
-                verdict = false;
-            }
-        }
-        return verdict;
+        return false;
     }
 }
